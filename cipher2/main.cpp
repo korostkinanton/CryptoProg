0a5d82c1b4310db9db7ff66edc6e542e3e9e40c4
korostkinanton <akorostkin1@gmail.com>
2023-12-14 21:24:34 +0300

    nakonecto

--- cipher2/main.cpp
+++ cipher2/main.cpp
@@ -1,77 +1,81 @@
 #include <iostream>
 #include <fstream>
 #include <cryptopp/aes.h>
-#include <cryptopp/modes.h>
+#include <cryptopp/ccm.h>
 #include <cryptopp/filters.h>
-#include <cryptopp/base64.h>
+#include <cryptopp/files.h>
+#include <cryptopp/modes.h>
+#include <cryptopp/secblock.h>
+#include <cryptopp/cryptlib.h>
 #include <cryptopp/osrng.h>
-#include <cryptopp/pwdbased.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/md5.h>
 
-void encryptFile(const std::string& inputFile, const std::string& outputFile, const std::string& password)
-{
-    CryptoPP::SecByteBlock key(CryptoPP::AES::MAX_KEYLENGTH);
-    CryptoPP::SecByteBlock iv(CryptoPP::AES::BLOCKSIZE);
+using namespace CryptoPP;
 
-    CryptoPP::PKCS5_PBKDF2_HMAC<CryptoPP::SHA1> pbkdf;
-    pbkdf.DeriveKey(key, key.size(), 0, (CryptoPP::byte*)password.data(), password.size(), iv, iv.size(), 1000);
+void DeriveKey(const std::string& password, SecByteBlock& derivedKey, SecByteBlock& iv) {
+    const int KEY_SIZE = AES::DEFAULT_KEYLENGTH;
+    const int IV_SIZE = AES::BLOCKSIZE;
 
-    CryptoPP::CBC_Mode<CryptoPP::AES>::Encryption enc;
-    enc.SetKeyWithIV(key, key.size(), iv);
+    MD5 hash; //Создается объект MD5, который будет использоваться для вычисления хеша пароля.
+    SecByteBlock digest(hash.DigestSize()); //Создается блок для хранения хеша пароля (digest)
+    hash.Update((const byte*)password.data(), password.size());
+    hash.Final(digest);
+    //Пароль обновляется в объекте MD5.
 
-    std::ifstream ifs(inputFile, std::ios::binary);
-    std::ofstream ofs(outputFile, std::ios::binary);
-
-    CryptoPP::FileSource(ifs, true, new CryptoPP::StreamTransformationFilter(enc, new CryptoPP::FileSink(ofs)));
+    std::memcpy(derivedKey, digest, KEY_SIZE);
+    std::memcpy(iv, digest + KEY_SIZE, IV_SIZE);
+    //Полученный хеш пароля используется для заполнения ключа и IV с помощью функции memcpy.
 }
 
-void decryptFile(const std::string& inputFile, const std::string& outputFile, const std::string& password)
-{
-    CryptoPP::SecByteBlock key(CryptoPP::AES::MAX_KEYLENGTH);
-    CryptoPP::SecByteBlock iv(CryptoPP::AES::BLOCKSIZE);
+void EncryptFile(const std::string& inputFile, const std::string& outputFile, const std::string& password) {
+    SecByteBlock key(AES::DEFAULT_KEYLENGTH);
+    SecByteBlock iv(AES::BLOCKSIZE);
+	// Создаются объекты key и iv типа SecByteBlock для хранения ключа и вектора инициализации.
+    DeriveKey(password, key, iv); //Функция DeriveKey вызывается для генерации ключа и вектора инициализации на основе пароля.
 
-    CryptoPP::PKCS5_PBKDF2_HMAC<CryptoPP::SHA1> pbkdf;
-    pbkdf.DeriveKey(key, key.size(), 0, (CryptoPP::byte*)password.data(), password.size(), iv, iv.size(), 1000);
+    CBC_Mode<AES>::Encryption encryption; //Создается объект CBC_Mode<AES>::Encryption encryption для представления шифрования в режиме CBC.
+    encryption.SetKeyWithIV(key, key.size(), iv); //Устанавливаются ключ и вектор инициализации для объекта encryption с помощью метода SetKeyWithIV
 
-    CryptoPP::CBC_Mode<CryptoPP::AES>::Decryption dec;
-    dec.SetKeyWithIV(key, key.size(), iv);
-
-    std::ifstream ifs(inputFile, std::ios::binary);
-    std::ofstream ofs(outputFile, std::ios::binary);
-
-    CryptoPP::FileSource(ifs, true, new CryptoPP::StreamTransformationFilter(dec, new CryptoPP::FileSink(ofs)));
+    FileSource fs(inputFile.c_str(), true, new StreamTransformationFilter(encryption, new FileSink(outputFile.c_str()))); //Создается объект FileSource для чтения исходного файла и объект FileSink для записи зашифрованных данных. Запускается процесс шифрования с использованием объекта encryption и созданных объектов FileSource и FileSink
 }
 
-int main()
-{
-    std::string inputFile, outputFile, password;
+void DecryptFile(const std::string& inputFile, const std::string& outputFile, const std::string& password) {
+    SecByteBlock key(AES::DEFAULT_KEYLENGTH);
+    SecByteBlock iv(AES::BLOCKSIZE);
+
+    DeriveKey(password, key, iv);
+
+    CBC_Mode<AES>::Decryption decryption; //Создается объект CBC_Mode<AES>::Decryption decryption для представления расшифрования в режиме CBC.
+    decryption.SetKeyWithIV(key, key.size(), iv); //Устанавливаются ключ и вектор инициализации для объекта decryption с помощью метода SetKeyWithIV
+
+    FileSource fs(inputFile.c_str(), true, new StreamTransformationFilter(decryption, new FileSink(outputFile.c_str()))); //Запускается процесс расшифрования с использованием объекта decryption и созданных объектов FileSource и FileSink
+}
+
+int main() {
     int choice;
+    std::string inputFile, outputFile, password;
 
-    std::cout << "Выберите режим работы (1 - зашифрование, 2 - расшифрование): ";
+    std::cout << "1 - зашифровка, 2 - расшировка ";
     std::cin >> choice;
 
-    std::cout << "Введите имя входного файла: ";
+    std::cout << "Введите имя входного файла ";
     std::cin >> inputFile;
 
-    std::cout << "Введите имя выходного файла: ";
+    std::cout << "Введите имя выходного файла ";
     std::cin >> outputFile;
 
-    std::cout << "Введите пароль: ";
+    std::cout << "Введите пароль ";
     std::cin >> password;
 
-    if (choice == 1)
-    {
-        encryptFile(inputFile, outputFile, password);
-        std::cout << "Файл зашифрован." << std::endl;
-    }
-    else if (choice == 2)
-    {
-        decryptFile(inputFile, outputFile, password);
-        std::cout << "Файл расшифрован." << std::endl;
-    }
-    else
-    {
-        std::cout << "Неправильный выбор режима работы." << std::endl;
-        return 1;
+    if (choice == 1) {
+        EncryptFile(inputFile, outputFile, password);
+        std::cout << "Файл зашифрован" << std::endl;
+    } else if (choice == 2) {
+        DecryptFile(inputFile, outputFile, password);
+        std::cout << "Файл расшифрован" << std::endl;
+    } else {
+        std::cout << "Некоректный режим работы" << std::endl;
     }
 
     return 0;
